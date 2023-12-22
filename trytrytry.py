5f3099c22db84ec2889b5d064b2bb1ac5e1e4468
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2023-12-22 23:29:54 +0800

    Update trytrytry.py
    finalization

--- trytrytry.py
+++ trytrytry.py
@@ -4,12 +4,11 @@ import mediapipe as mp
 import sys
 
 width, height = (500, 500)
-cap = cv2.VideoCapture("video.mp4")
-result1 = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'MP4V'), 25, (500, 500))
+cap = cv2.VideoCapture(0)
+result1 = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 25, (500, 500))
 
 cap.set(3, width)
 cap.set(4, height)
-
 
 image = cv2.imread("hehe.jpg")
 
@@ -19,16 +18,17 @@ mp_face_mesh = mp.solutions.face_mesh
 
 def get_landmark_position(src_image):
     with mp_face_mesh.FaceMesh(
-            static_image_mode= True,
-            max_num_faces= 1,
-            refine_landmarks= True,
-            min_detection_confidence= 0.5) as face_mesh:
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5
+    ) as face_mesh:
         results = face_mesh.process(cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB))
 
         if not results.multi_face_landmarks:
             return None
-        
-        if len(results.multi_face_landmarks > 1):
+
+        if len(results.multi_face_landmarks) > 1:
             sys.exit("There are too many face landmarks.")
 
         src_face_landmark = results.multi_face_landmarks[0].landmark
@@ -39,8 +39,8 @@ def get_landmark_position(src_image):
             x = int(src_face_landmark[i].x * src_image.shape[1])
             landmark_points.append((x, y))
 
-            return landmark_points
-        
+        return landmark_points
+
 def extract_index_nparray(nparray):
     index = None
     for num in nparray[0]:
@@ -54,20 +54,20 @@ def get_triangles(convexhull, landmark_points, np_points):
     subdiv.insert(landmark_points)
     triangles = subdiv.getTriangleList()
     triangles = np.array(triangles, dtype=np.int32)
-    
+
     indexes_triangles = []
     for t in triangles:
         pt1 = (t[0], t[1])
         pt2 = (t[2], t[3])
         pt3 = (t[4], t[5])
 
-        index_pt1 = np.where((np_points == pt1).all(axis = 1))
+        index_pt1 = np.where((np_points == pt1).all(axis=1))
         index_pt1 = extract_index_nparray(index_pt1)
 
-        index_pt2 = np.where((np_points == pt2).all(axis = 1))
+        index_pt2 = np.where((np_points == pt2).all(axis=1))
         index_pt2 = extract_index_nparray(index_pt2)
 
-        index_pt3 = np.where((np_points == pt3).all(axis = 1))
+        index_pt3 = np.where((np_points == pt3).all(axis=1))
         index_pt3 = extract_index_nparray(index_pt3)
 
         if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
@@ -76,13 +76,13 @@ def get_triangles(convexhull, landmark_points, np_points):
 
     return indexes_triangles
 
-
 def triangulation(triangle_index, landmark_points, img=None):
     tr1_pt1 = landmark_points[triangle_index[0]]
     tr1_pt2 = landmark_points[triangle_index[1]]
     tr1_pt3 = landmark_points[triangle_index[2]]
-    
-    rect = cv2.boundingrect(triangle)
+    triangle = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
+
+    rect = cv2.boundingRect(triangle)
     (x, y, w, h) = rect
 
     cropped_triangle = None
@@ -90,19 +90,21 @@ def triangulation(triangle_index, landmark_points, img=None):
         cropped_triangle = img[y:y + h, x:x + w]
 
     cropped_triangle_mask = np.zeros((h, w), np.uint8)
+
     points = np.array([tr1_pt1[0] - x, tr1_pt1[1] - y],
-                     [tr1_pt2[0] - x, tr1_pt2[1] - y],
-                     [tr1_pt3[0] - x, tr1_pt3[1] - y], np.uint32)
+                      [tr1_pt2[0] - x, tr1_pt2[1] - y],
+                      [tr1_pt3[0] - x, tr1_pt3[1] - y], np.int32)
 
     cv2.fillConvexPoly(cropped_triangle_mask, points, 255)
+
     return points, cropped_triangle, cropped_triangle_mask, rect
 
-def warped_triangle(rect, points1, points2, src_cropped_triangle, dest_cropped_triangle_mask):
+def warp_triangle(rect, points1, points2, src_cropped_triangle, dest_cropped_triangle_mask):
     (x, y, w, h) = rect
 
-    matrix = cv2. getAffineTransform(np.float32(points1), np.float32(points2))
+    matrix = cv2.getAffineTransform(np.float32(points1), np.float32(points2))
     warped_triangle = cv2.warpAffine(src_cropped_triangle, matrix, (w, h))
-    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask= dest_cropped_triangle_mask)
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=dest_cropped_triangle_mask)
 
     return warped_triangle
 
@@ -113,25 +115,45 @@ def add_piece_of_new_face(new_face, rect, warped_triangle):
 
     _, mask_triangles_designed = cv2.threshold(new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
 
-    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask= mask_triangles_designed)
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
 
     new_face_rect_area = cv2.add(new_face_rect_area, warped_triangle)
     new_face[y:y + h, x:x + w] = new_face_rect_area
 
+def clone_faces(dest_image, new_face, head_mask, center_face):
+    min_height = min(dest_image.shape[0], new_face.shape[0], head_mask.shape[0])
+    min_width = min(dest_image.shape[1], new_face.shape[1], head_mask.shape[1])
+
+    dest_image = dest_image[:min_height, :min_width]
+    new_face = new_face[:min_height, :min_width]
+    head_mask = head_mask[:min_height, :min_width]
+
+    head_mask = head_mask.astype(float) / 255.0
+
+    result = (1 - head_mask[:, :, np.newaxis]) * dest_image + head_mask[:, :, np.newaxis] * new_face
+    result = result.astype(np.uint8)
+
+    return result
+
+
 def swap_new_face(dest_image, dest_image_gray, dest_convexHull, new_face):
     face_mask = np.zeros_like(dest_image_gray)
-    head_mask = cv2.fillconvexPoly(face_mask, dest_convexHull, 255)
+    head_mask = cv2.fillConvexPoly(face_mask, dest_convexHull, 255)
     face_mask = cv2.bitwise_not(head_mask)
 
-    head_without_face = cv2.bitwise_and(dest_image, dest_image, mask= face_mask)
+    head_without_face = cv2.bitwise_and(dest_image, dest_image, mask=face_mask)
 
-    result = cv2.add(head_without_face, new_face)
+    center_face = (0, 0)
 
-    (x, y, w, h) = cv2.boundingRect(dest_convexHull)
-    center_face = (int((x + x + w) / 2), int((y + y + h) / 2))
+    small_dest_image = cv2.resize(dest_image, (width // 2, height // 2))
+    small_new_face = cv2.resize(new_face, (width // 2, height // 2))
+    small_head_mask = cv2.resize(head_mask, (width // 2, height // 2))
 
-    return cv2.seamlessClone(result, dest_image, head_mask, center_face, cv2.MIXED_CLONE)
+    small_result = clone_faces(small_dest_image, small_new_face, small_head_mask, center_face)
 
+    result = cv2.resize(small_result, (width, height))
+
+    return result
 
 def set_src_image(image):
     global src_image, src_image_gray, src_mask, src_landmark_points, src_np_points, src_convexHull, indexes_triangles
@@ -145,17 +167,14 @@ def set_src_image(image):
     src_convexHull = cv2.convexHull(src_np_points)
     cv2.fillConvexPoly(src_mask, src_convexHull, 255)
 
-    indexes_triangle = get_triangles(convexhull=src_convexHull,
-                                     landmark_points= src_landmark_points,
-                                     np_points=src_np_points)
     
+    indexes_triangle = get_triangles(convexhull=src_convexHull,landmark_points=src_landmark_points,np_points=src_np_points)
 
+indexes_triangles = []
 set_src_image(image)
 
 
 while True:
-    global src_image, src_image_gray, src_mask, src_landmark_points, src_np_points, src_convexHull, indexes_triangles
-
     _, dest_image = cap.read()
     dest_image = cv2.resize(dest_image, (width, height))
 
@@ -173,33 +192,31 @@ while True:
     new_face = np.zeros((height, width, channels), np.uint8)
 
     for triangle_index in indexes_triangles:
-
         points, src_cropped_triangle, cropped_triangle_mask, _ = triangulation(
-            triangle_index= triangle_index,
-            landmark_points= src_landmark_points,
-            img= src_image
+            triangle_index=triangle_index,
+            landmark_points=src_landmark_points,
+            img=src_image
         )
 
         points2, _, dest_cropped_triangle_mask, rect = triangulation(
-            triangle_index= triangle_index,
-            landmark_points= dest_landmark_points
+            triangle_index=triangle_index,
+            landmark_points=dest_landmark_points
         )
 
-        warped_triangle = warp_triangle(rect=rect, points1= points, points2= points2, 
-                                        src_cropped_triangle= src_cropped_triangle,
-                                        dest_cropped_triangle_mask= dest_cropped_triangle_mask)
-        
-        add_piece_of_new_face(new_face= new_face, rect= rect, warped_triangle= warped_triangle)
-        
-    result = swap_new_face(dest_image= dest_image, dest_image_gray= dest_image_gray, 
-                            dest_convexHull= dest_convexHull, new_face= new_face)
-        
+        warped_triangle = warp_triangle(rect=rect, points1=points, points2=points2,
+                                        src_cropped_triangle=src_cropped_triangle,
+                                        dest_cropped_triangle_mask=dest_cropped_triangle_mask)
+
+        add_piece_of_new_face(new_face=new_face, rect=rect, warped_triangle=warped_triangle)
+
+    result = swap_new_face(dest_image=dest_image, dest_image_gray=dest_image_gray,
+                            dest_convexHull=dest_convexHull, new_face=new_face)
+
     result = cv2.medianBlur(result, 3)
-    h, w, _= src_image.shape
+    h, w, _ = src_image.shape
     rate = width / w
 
-
-    cv2.imshow("Source image", cv2.resize(src_image,(int(w * rate), int(h * rate))))
+    cv2.imshow("Source image", cv2.resize(src_image, (int(w * rate), int(h * rate))))
     cv2.imshow("New face", new_face)
 
     result1.write(result)
@@ -210,3 +227,5 @@ while True:
         break
 
 cv2.destroyAllWindows()
+cap.release()
+result1.release()
